a660412a7c7647e90177fd7b53cda86f4999449f
mayurib2 <bhisemayuri@gmail.com>
2019-11-27 19:20:30 -0800

    Update tests to use axios

--- test/test.js
+++ test/test.js
@@ -7,23 +7,25 @@ const expect = require('chai').expect;
 require("../index.js");
 
 it('Check get business by business id is working', function(done) {
-  request('http://localhost:3001/businesses/02151fdb-8b50-4068-b8a1-1d24ed425c59', function (error, response, body) {
-    expect(response.statusCode).to.equal(200);
-
-    const bodyJson = JSON.parse(response.body);
-    expect(bodyJson.length).to.be.above(0);
-    done();
-  });
+    axios.get('http://localhost:3001/businesses/02151fdb-8b50-4068-b8a1-1d24ed425c59')
+        .then((res) => {
+            expect(res.status).to.equal(200);
+            done();
+        })
+        .catch((error) => {
+            console.error(error)
+            done();
+        })
 });
 
 it('Check search business by event type is working', function (done) {
-  axios.get('http://localhost:3001/businesses?event_type=restaurant&city=Danville')
-      .then((res) => {
-        expect(res.status).to.equal(200);
-        done();
-      })
-      .catch((error) => {
-        console.error(error)
-        done();
-      })
+    axios.get('http://localhost:3001/businesses?event_type=restaurant&city=Danville')
+        .then((res) => {
+            expect(res.status).to.equal(200);
+            done();
+        })
+        .catch((error) => {
+            console.error(error)
+            done();
+        })
 });
